a3f46642dbd18addb3865ce19cb99a5dda158520
Francisco José A. C. Souza <francisceioseph@gmail.com>
2015-12-25 15:38:46 -0300

    first commit

---
+++ Swift-Files/FileHelper.swift
@@ -0,0 +1,87 @@
+//
+//  FileHelper.swift
+//  Swift-Files
+//
+//  Created by Francisco José A. C. Souza on 24/12/15.
+//  Copyright © 2015 Francisco José A. C. Souza. All rights reserved.
+//
+
+import UIKit
+
+class FileHelper: NSObject {
+    static let sharedInstance = FileHelper()
+    
+    let fileManager = NSFileManager.defaultManager()
+    
+    func createPlainTextFileWithName(name: String) -> Bool{
+        let filePath = self.documentsFolder().stringByAppendingString("/\(name).txt")
+        
+        return fileManager.createFileAtPath(filePath, contents: nil , attributes: nil)
+    }
+    
+    func readPlainTextWithName(name: String) throws -> String? {
+        let filePath = self.documentsFolder().stringByAppendingString("/\(name).txt")
+        let fileContent:String? = try String(contentsOfFile: filePath)
+        
+        return fileContent
+    }
+    
+    func savePlainTextFileWithName(name: String, fileContent: String) -> Bool {
+        let filePath = self.documentsFolder().stringByAppendingString("/\(name).txt")
+        let fileData = fileContent.dataUsingEncoding(NSUTF8StringEncoding)
+        
+        return fileManager.createFileAtPath(filePath, contents: fileData , attributes: nil)
+    }
+    
+    func deletePlainTextFileWithName(name: String) throws{
+        let filePath = self.documentsFolder().stringByAppendingString("/\(name).txt")
+        try fileManager.removeItemAtPath(filePath)
+    }
+    
+    //MARK: - Plist Text File
+
+    func createPlistFileWithName(name: String) -> Bool{
+        let filePath = self.documentsFolder().stringByAppendingString("/\(name).plist")
+        
+        return fileManager.createFileAtPath(filePath, contents: nil , attributes: nil)
+    }
+    
+    func readPlistWithName(name: String) -> NSDictionary? {
+        let filePath = self.documentsFolder().stringByAppendingString("/\(name).plist")
+        let fileContent:NSDictionary? = NSDictionary(contentsOfFile: filePath)
+        
+        return fileContent
+    }
+    
+    func savePlistFileWithName(name: String, fileContent: NSDictionary) -> Bool {
+        let filePath = self.documentsFolder().stringByAppendingString("/\(name).plist")
+        let fileData = NSKeyedArchiver.archivedDataWithRootObject(fileContent)
+        
+        return fileManager.createFileAtPath(filePath, contents: fileData , attributes: nil)
+    }
+    
+    func deletePlistFileWithName(name: String) throws{
+        let filePath = self.documentsFolder().stringByAppendingString("/\(name).plist")
+        try fileManager.removeItemAtPath(filePath)
+    }
+
+    //MARK: - list files into a documents directory
+    
+    func listDocumentsFolderContent() throws -> [String]{
+        let directoryContents: [String]? = try fileManager.contentsOfDirectoryAtPath(documentsFolder())
+        var fileNames: [String] = []
+        
+        if let directoryContents = directoryContents {
+            fileNames = directoryContents.map({ (path:String) -> String in
+                path.componentsSeparatedByString("/").last!
+            })
+        }
+        
+        return fileNames
+    }
+    
+    private func documentsFolder () -> String{
+        let folders = NSSearchPathForDirectoriesInDomains(.DocumentDirectory, .UserDomainMask, true)
+        return folders.first!
+    }
+}
---
+++ Swift-Files/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  Swift-Files
+//
+//  Created by Francisco José A. C. Souza on 24/12/15.
+//  Copyright © 2015 Francisco José A. C. Souza. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+}
+
